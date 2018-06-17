cbf6f6b500e704d1a11589185282e8a02d146708
Michael Davids <michaelcdavids@gmail.com>
2018-06-17 14:41:27 +0200

    fixed failing tests

--- test/shoe-catalogue-factory-test.js
+++ test/shoe-catalogue-factory-test.js
@@ -1,40 +1,36 @@
 describe('The getShoes Function',function(){
+
    it('should fetch the shoe data',function(){
       var shoeData = ShoeCatalogue();
       assert.deepEqual(shoeData.shoesInStock(),
-      [  {color : 'white', brand : "Nike", price : 1350, size : 6, in_stock : 3},
-         {color : 'green', brand : "Adidas", price : 1350, size : 7,in_stock : 25},
-         {color : 'black', brand : "Puma", price : 1350, size : 7,in_stock : 12},
-         {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 3},
-         {color : 'orange', brand : "Vans", price : 350, size : 6,in_stock : 50},
-         {color : 'white', brand : "Nike", price : 15000, size : 10,in_stock : 1},
-         {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 2},
-         {color : 'red', brand : "Reebok", price : 750, size : 10,in_stock : 10},
-         {color : 'blue', brand : "Vans", price : 1000, size : 19,in_stock : 8},
-         {color : 'white', brand : "Nike", price : 1350, size : 6, in_stock : 3},
-         {color : 'green', brand : "Adidas", price : 1350, size : 7,in_stock : 25},
-         {color : 'black', brand : "Puma", price : 1350, size : 7,in_stock : 12},
-         {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 3},
-         {color : 'orange', brand : "Vans", price : 350, size : 16,in_stock : 50},
-         {color : 'white', brand : "Nike", price : 15000, size : 10,in_stock : 1},
-         {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 2},
-         {color : 'red', brand : "Reebok", price : 750, size : 10,in_stock : 10},
-         {color : 'blue', brand : "Vans", price : 1000, size : 19,in_stock : 8}
+      [
+         {id:1,color : 'white', brand : "Nike", price : 1350, size : 1, in_stock : 3},
+         {id:2,color : 'green', brand : "Adidas", price : 1999, size : 1,in_stock : 25},
+         {id:3,color : 'black', brand : "Lacoste", price : 1540, size : 2,in_stock : 12},
+         {id:4,color : 'orange', brand : "Nike", price : 1350, size : 2,in_stock : 3},
+         {id:5,color : 'orange', brand : "Vans", price : 1999, size : 3,in_stock : 50},
+         {id:6,color : 'white', brand : "Nike", price : 11200, size : 3,in_stock : 1},
+         {id:7,color : 'orange', brand : "Nike", price : 1350, size : 4,in_stock : 2},
+         {id:8,color : 'red', brand : "Fila", price : 750, size : 4,in_stock : 10},
+         {id:9,color : 'blue', brand : "Vans", price : 2999, size : 5,in_stock : 8},
+         {id:10,color : 'white', brand : "Fila", price : 5000, size : 5, in_stock : 3}
       ]);
    });
 });
+
 describe('The filterShoes Function',function(){
+
    it('should return the shoes that matches the search criteria',function(){
       var shoeData = ShoeCatalogue();
-      assert.deepEqual(shoeData.filteredShoes("orange",8,"Nike"),[
-        {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 3},
-        {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 2},
-        {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 3},
-        {color : 'orange', brand : "Nike", price : 1350, size : 8,in_stock : 2},
-      ]);
+      assert.deepEqual(shoeData.filteredShoes("white",1,"Nike"),[
+         {id:1,color : 'white', brand : "Nike", price : 1350, size : 1, in_stock : 3}]);
    });
+
 });
+
+
 describe('The addItemToCart Function',function(){
+
    it('should add item to cart and decrement stock of the item',function(){
       var shoeData = ShoeCatalogue();
 
@@ -51,6 +47,7 @@ describe('The addItemToCart Function',function(){
          {id:9,color : 'blue', brand : "Vans", price : 2999, size : 5,in_stock : 8},
          {id:10,color : 'white', brand : "Fila", price : 5000, size : 5, in_stock : 2}]);
    });
+
    it('should add item to cart and decrement stock of the item',function(){
       var shoeData = ShoeCatalogue();
 
@@ -68,29 +65,12 @@ describe('The addItemToCart Function',function(){
          {id:10,color : 'white', brand : "Fila", price : 5000, size : 5, in_stock : 3}]);
    });
 });
+
+
 describe('the Clear Function',function(){
    it('shoud empty the shopping cart and clear the cart total',function(){
       var shoeData = ShoeCatalogue();
-
-      shoeData.addStockItem('white',"Converse",1899,7,2);
-      shoeData.addStockItem('beige',"Caterpillar",2799,8,2);
-      shoeData.addStockItem('blue',"Asics",899,5,9);
-      shoeData.addStockItem('white',"Puma",1999,7,2);
-      shoeData.addStockItem('white',"Nike",999,7,1);
       shoeData.Clear();
-
       assert.deepEqual(shoeData.Cart(),[]);
    });
 });
-// describe('',function(){
-//    it('',function(){
-//       var instance = ShoeCatalogue();
-//       assert.equal(instance.functionName(),);
-//    });
-// });
-// describe('',function(){
-//    it('',function(){
-//       var instance = ShoeCatalogue();
-//       assert.equal(instance.functionName(),);
-//    });
-// });
